e0a71b27e507e5b1ffedb185ca5ac635bff5abcd
Monnavarro <montserrat.nvro.lpz@gmail.com>
2022-07-13 18:12:34 +0200

    feat: This file showcases how to store multiple values together

---
+++ python-fundamentals/operators-in-python/grouping-values.py
@@ -0,0 +1,26 @@
+# -- coding: utf-8 --
+"""
+
+Created on: 13/7/22
+@author: Montse Navarro <montserrat.nvro.lpz@gmail.com>
+Licence,
+"""
+# Making a List #
+
+my_list = [1, 2.5, "A string", True]
+print(my_list)
+
+
+print(my_list[2])
+print(len(my_list))
+
+my_string = "0123456789"
+print(my_string[-2: -6: -2])
+
+x = 20
+y = 5
+result = (x + True) / (4 - y * False)
+print(result)
+
+A = (4 - y * False)
+print(A)

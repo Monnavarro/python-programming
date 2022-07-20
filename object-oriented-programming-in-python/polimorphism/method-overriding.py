1857f8f907d9175083ccae83caa3d547ff47c473
Monnavarro <montserrat.nvro.lpz@gmail.com>
2022-07-20 19:46:09 +0200

    feat: this file contains examples about method overriding is and how to achieve it in Python.

---
+++ object-oriented-programming-in-python/polimorphism/method-overriding.py
@@ -0,0 +1,39 @@
+# -- coding: utf-8 --
+"""
+
+Created on: 20/7/22
+@author: Montse Navarro <montserrat.nvro.lpz@gmail.com>
+Licence,
+"""
+class Shape:
+    def __init__(self): # initializing sides of all shapes to 0
+        self.sides = 0
+
+    def getArea(self):
+        pass
+
+class Rectangle(Shape): # derived form Shape class
+    # initializer
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        self.sides = 4
+
+    # method to calculate Area
+    def getArea(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    # initializer
+    def __init__(self, radius=0):
+        self.radius = radius
+
+     # method to calculate Area
+    def getArea(self):
+        return self.radius * self.radius * 3.142
+
+shapes = [Rectangle(6,10), Circle(7)]
+print("Area of rectangle is:", str(shapes[0].getArea()))
+print("Area of rectangle is:", str(shapes[1].getArea()))
+
+
